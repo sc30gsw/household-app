5d0d6c013b6780ccba488546886a95fd0ed24528
sc30gsw <haidoushi875@gmail.com>
2022-03-20 14:00:25 +0900

    ユーザーマスタのエンティティクラスの作成

---
+++ src/main/java/com/example/demo/domain/entity/MUser.java
@@ -0,0 +1,53 @@
+package com.example.demo.domain.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+/**
+ * ユーザーマスタエンティティクラス
+ */
+@Data
+public class MUser implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	/**ユーザーID*/
+	private Integer userId;
+	
+	/**ユーザー名*/
+	private String username;
+	
+	/**メールアドレス*/
+	private String email;
+	
+	/**パスワード*/
+	private String password;
+	
+	/**パスワード確認用*/
+	private String passwordConfirm;
+	
+	/**年齢*/
+	private Integer age;
+	
+	/**性別*/
+	private String sex;
+	
+	/**作成日時*/
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
+	private LocalDateTime createDate;
+	
+	/**更新日時*/
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
+	private LocalDateTime updateDate;
+	
+	/**削除日時*/
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
+	private LocalDateTime deleteDate;
+	
+	/**削除フラグ*/
+	private String deleteFlg;
+}
