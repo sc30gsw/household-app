158d6c2d6ecbf52dec87f55eed3b42d67bef845d
sc30gsw <haidoushi875@gmail.com>
2022-03-20 16:23:07 +0900

    メールアドレスをキーにユーザーを検索する処理を追加

--- src/main/java/com/example/demo/domain/mapper/MUserMapper.java
+++ src/main/java/com/example/demo/domain/mapper/MUserMapper.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.mapper;
 
+import java.util.Optional;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.demo.domain.entity.MUser;
@@ -16,4 +18,12 @@ public interface MUserMapper {
 	 * @param user ユーザーマスタ
 	 */
 	public int insertMUser(MUser user);
+	
+	/**
+	 * メールアドレスでユーザーを検索する処理
+	 * 
+	 * @param email メールアドレス
+	 * @return ユーザー1件
+	 */
+	public Optional<MUser> findByEmail(String email);
 }
