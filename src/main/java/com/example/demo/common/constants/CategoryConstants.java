2345b019b2e4f35f57cea97b143f14520e6b7109
sc30gsw <haidoushi875@gmail.com>
2022-04-04 08:59:17 +0900

    カテゴリー共通定数クラスの作成

---
+++ src/main/java/com/example/demo/common/constants/CategoryConstants.java
@@ -0,0 +1,42 @@
+package com.example.demo.common.constants;
+
+/**
+ * カテゴリー共通定数クラス
+ */
+public class CategoryConstants {
+
+	/**
+	 * プライベートコンストラクタ
+	 */
+	private CategoryConstants(){}
+	
+	public static final String FOOD_EXPENSE = "食費";
+	
+	public static final String COMMODITY = "日用品";
+	
+	public static final String HOBBIES = "趣味・娯楽";
+	
+	public static final String SOCIAL_EXPENSE = "交際費";
+	
+	public static final String TRANSPORTATION_EXPENSE = "交通費";
+	
+	public static final String FASSHON = "衣服・美容";
+	
+	public static final String CAR = "自動車";
+	
+	public static final String HEALTH_CARE = "健康・医療";
+	
+	public static final String LIBERAL_ARTS = "教養・教育";
+	
+	public static final String SPECIAL_EXPENSE = "特別な支出";
+	
+	public static final String UTILITIES_BILLS = "水道・光熱費";
+	
+	public static final String COMMUNICATION_COST = "通信費";
+	
+	public static final String HOUSEING_COST = "住宅";
+	
+	public static final String OTHERS_COST = "その他";
+	
+	public static final String UNSORTED = "未分類";
+}
