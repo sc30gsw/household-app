015027afd382bedce9d7acff0ba149eccf698efe
sc30gsw <haidoushi875@gmail.com>
2022-04-03 13:25:19 +0900

    月次家計簿取得処理の追加

--- src/main/java/com/example/demo/domain/repository/MHouseholdRepository.java
+++ src/main/java/com/example/demo/domain/repository/MHouseholdRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.domain.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.app.MHousehold.controller.MHouseholdCondition;
 import com.example.demo.domain.entity.MHousehold;
 import com.example.demo.domain.mapper.MHouseholdMapper;
 
@@ -23,5 +26,25 @@ public class MHouseholdRepository {
 	 */
 	public void registMHousehold(MHousehold household) {
 		mapper.insertMHousehold(household);
+	}
+	
+	/**
+	 * 月次家計簿取得処理
+	 * 
+	 * @param condition 家計簿検索条件
+	 * @return 家計簿マスタリスト
+	 */
+	public List<MHousehold> monthlyGetHousehold(MHouseholdCondition condition) {
+		return mapper.monthlyHousehold(condition);
+	}
+	
+	/**
+	 * 月次家計簿集計取得処理
+	 * 
+	 * @param condition 家計簿検索条件
+	 * @return 家計簿マスタ
+	 */
+	public MHousehold monthlyGetSumHousehold(MHouseholdCondition condition) {
+		return mapper.monthlySumHousehold(condition);
 	}
 }
